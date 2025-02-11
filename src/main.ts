a8113abebbe4b7c86e81350115d9d5ec0afc95c8
yujch716 <70509403+yujch716@users.noreply.github.com>
2025-02-12 04:06:53 +0900

    feature: logs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+  });
   await app.listen(8001);
 }
 bootstrap();
