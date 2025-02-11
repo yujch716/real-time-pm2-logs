d165260ae1803118b698914353d401ad00867937
yujch716 <70509403+yujch716@users.noreply.github.com>
2025-02-12 05:12:23 +0900

    feature: logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('logs')
+  getLogs(): string {
+    //const logFilePath = join(process.cwd(), 'test.log');
+    const logFilePath = '/home/ubuntu/.pm2/logs/helloboard-server-out.log';
+    try {
+      const logData = readFileSync(logFilePath, 'utf-8');
+      return logData;
+    } catch (error) {
+      console.error(error);
+      return '로그 파일을 읽는 데 실패했습니다.';
+    }
   }
 }
